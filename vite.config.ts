cbb3925389ef37948c6a02043657acb57bc9561f
Paul David <puddavid@gmail.com>
2026-01-19 12:15:44 -0500

    Fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hebrew-training/',
+  base: '/hebrew_training/',
   plugins: [vue()],
   resolve: {
     alias: {
